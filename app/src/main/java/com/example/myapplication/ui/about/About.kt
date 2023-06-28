7ada9a2018aac2d198a2ddf04bb5141a6d28e6cd
SIRAMRULWA <sihlemrulwa@gmail.com>
2023-06-28 15:25:29 +0200

    I added image

---
+++ app/src/main/java/com/example/myapplication/ui/about/About.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.about
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class about : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/about/AboutViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.ui.about
+
+class AboutViewModel {
+}
